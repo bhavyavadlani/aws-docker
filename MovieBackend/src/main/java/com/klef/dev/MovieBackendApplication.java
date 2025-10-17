93ef747a9d832e14ddad9ab3358a6f1d45527b11
2300033794 <2300033794@kluniversity.in>
2025-10-17 18:45:41 +0530

    v2.0

--- MovieBackend/src/main/java/com/klef/dev/MovieBackendApplication.java
+++ MovieBackend/src/main/java/com/klef/dev/MovieBackendApplication.java
@@ -2,9 +2,10 @@ package com.klef.dev;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MovieBackendApplication {
+public class MovieBackendApplication extends SpringBootServletInitializer{
 
     public static void main(String[] args) {
         SpringApplication.run(MovieBackendApplication.class, args);
